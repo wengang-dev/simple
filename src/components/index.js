bcf6d4e39679258110b73927bcc3433e76c56e0a
huas_wg <1240374391@qq.com>
2019-02-14 00:35:09 +0800

    merge radio components conflict

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './index.less'
 
 import button from './button'
@@ -27,31 +26,3 @@ export default {
   install,
   ...components
 }
-=======
-import './index.less'
-
-import button from './button'
-import input from './input'
-
-const components = {
-  button,
-  input
-}
-
-const install = function(Vue) {
-  if (install.installed) return
-  // components.map(component => Vue.component(component.name, component))
-  Object.keys(components).forEach(key => {
-    Vue.component(components[key].name, components[key])
-  })
-}
-
-if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue)
-}
-
-export default {
-  install,
-  ...components
-}
->>>>>>> 47f944f15ec30873c4759cf1ceff4a72b4fd6800
--- src/components/input/input.vue
+++ src/components/input/input.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <template>
   <div class="inputContainer">
     <slot name="before">test slotaaa</slot>
@@ -76,69 +75,3 @@ export default {
   font-size: 0px;
 }
 </style>
-=======
-<template>
-  <div class="inputContainer">
-    <!-- sloted?'':sloted=='before'?'sim-input-before':'sim-input-after' -->
-    <slot name="before">test slotaaa</slot>
-    <input class="sim-input "
-           :class="[
-                    disabled=='undefined'?'':'sim-input-disbaled',
-                    
-                  ]"
-           :type="type"
-           :value="value"
-           :placeholder="placeholder"
-           @input="input($event.target.value)"
-           :disabled="disabled">
-    <slot name="after"></slot>
-  </div>
-</template>
-
-<script>
-export default {
-  name: "simInput",
-  props: {
-    value: {
-      type: String | Number,
-      default: ""
-    },
-    placeholder: {
-      type: String
-    },
-    type: {
-      type: String,
-      default: "text"
-    },
-    disabled: {
-      type: Boolean,
-      default: false
-    },
-    slotBefore: {
-      type: String
-    },
-    slotAfter: {
-      type: String
-    }
-  },
-  data() {
-    return {};
-  },
-  methods: {
-    input(val) {
-      this.$emit("input", val);
-    }
-  },
-  mounted() {
-    console.log("slotBefore: ", this.slotBefore);
-    console.log("slotAfter: ", this.slotAfter);
-  }
-};
-</script>
-
-<style lang='less' scoped>
-.inputContainer {
-  display: inline-block;
-}
-</style>
->>>>>>> 47f944f15ec30873c4759cf1ceff4a72b4fd6800
