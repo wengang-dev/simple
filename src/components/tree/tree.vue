401c72cc8fbde66bf87ed262b3f2d54d06dcef59
huas_wg <1240374391@qq.com>
2019-02-26 21:05:12 +0800

    finish simple tree dynamic to difficult

--- src/components/tree/tree.vue
+++ src/components/tree/tree.vue
@@ -3,14 +3,18 @@
     <div v-for="(item,index) of data"
          class="sim-tree-levels"
          :key='item[defaultProps.label]'
-         @click="checkLevels(index,$event,item[defaultProps.children].length)">
-      <span class="sim-tree-label"><span v-if="item[defaultProps.children]"
+         @click="checkLevels(index,$event)">
+      <div class="sim-tree-label"
+           @click="click(item[defaultProps.label])">
+        <span v-if="item[defaultProps.children]"
               :class="[levelsShowState[index]?'sim-tree-label-show':'']"
-              class=" iconfont icon-solid-right-arrow sim-solid-right-arrow"></span>{{item[defaultProps.label]}}</span>
-      <sim-tree :data="item[defaultProps.children]"
-                v-show="secondaryShow[index]"
-                :style="{height:secondaryHeight[index]+'px'}"
-                :defaultProps="defaultProps"></sim-tree>
+              class=" iconfont icon-solid-right-arrow sim-solid-right-arrow"></span>
+        {{item[defaultProps.label]}}
+      </div>
+      <sim-tree class="sim-tree-secondary"
+                :data="item[defaultProps.children]"
+                :defaultProps="defaultProps"
+                v-show="secondaryShow[index]"></sim-tree>
     </div>
 
   </div>
@@ -36,50 +40,20 @@ export default {
   },
   components: {},
   methods: {
-    checkLevels(index, event, childrenLength) {
-      console.log("targetHeight", childrenLength);
-      let targetHeight = childrenLength * 25;
-      console.log(targetHeight);
+    checkLevels(index, event) {
       event.stopPropagation();
       event.cancelBubble = true;
       if (!this.levelsShowState[index]) {
         this.$set(this.levelsShowState, index, true);
         this.$set(this.secondaryShow, index, true);
-        requestAnimationFrame(() => {
-          this.secondaryLevelShow(index, targetHeight);
-        });
       } else {
         this.$set(this.levelsShowState, index, false);
-        requestAnimationFrame(() => {
-          this.secondaryLevelHidden(index);
-        });
-      }
-    },
-    secondaryLevelShow(index, targetHeight) {
-      //   显示动画
-      if (!this.secondaryHeight[index]) {
-        this.$set(this.secondaryHeight, index, 0);
-      }
-      if (this.secondaryHeight[index] < targetHeight) {
-        console.log("++++++++++++++");
-        this.$set(this.secondaryHeight, index, this.secondaryHeight[index] + 1);
-        requestAnimationFrame(() => {
-          this.secondaryLevelShow(index, targetHeight);
-        });
-      }
-    },
-    secondaryLevelHidden(index) {
-      //   隐藏动画
-      if (this.secondaryHeight[index] > 0) {
-        this.$set(this.secondaryHeight, index, this.secondaryHeight[index] - 1);
-
-        requestAnimationFrame(() => {
-          this.secondaryLevelHidden(index);
-        });
-      } else {
         this.$set(this.secondaryShow, index, false);
       }
     }
+  },
+  click(label) {
+    this.$emit("click", label);
   }
 };
 </script>
